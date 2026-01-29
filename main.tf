6898a3c6f021870d26624fc951821eb69e38a302
Akhila <akhichowdary00@gmail.com>
2026-01-29 12:19:03 +0530

    Jenkins

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.16.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "state-rem-86s"
+    key          = "jenkins"
+    use_lockfile = true
+    encrypt      = true
+    region       = "us-east-1"
+  }
+
+
+}
+
 provider "aws" {
   region = "us-east-1"
 }
